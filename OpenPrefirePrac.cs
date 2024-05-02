48df936bf3337236d1df63e7c80e3740865320c6
LynchMus <xubw@vip.qq.com>
2024-05-03 03:50:46 +0800

    Update OpenPrefirePrac.cs

--- OpenPrefirePrac.cs
+++ OpenPrefirePrac.cs
@@ -907,8 +907,8 @@ public class OpenPrefirePrac : BasePlugin
             && bot.PlayerPawn.Value != null
         )
         {
-            bot.PlayerPawn.Value.MoveType = MoveType_t.MOVETYPE_VPHYSICS;
-            Schema.SetSchemaValue(bot.PlayerPawn.Value.Handle, "CBaseEntity", "m_nActualMoveType", 5);
+            bot.PlayerPawn.Value.MoveType = MoveType_t.MOVETYPE_NONE;
+            Schema.SetSchemaValue(bot.PlayerPawn.Value.Handle, "CBaseEntity", "m_nActualMoveType", 0);
             Utilities.SetStateChanged(bot.PlayerPawn.Value, "CBaseEntity", "m_MoveType");
         }
     }
